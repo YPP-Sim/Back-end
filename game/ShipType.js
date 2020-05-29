bf12111bfd02009ccf2875100ac898cfeb0eae0e
Mark Artishuk <markyshuk@gmail.com>
2020-05-28 23:14:34 -0700

    Added ship stats and new war brig ship

--- game/ShipType.js
+++ game/ShipType.js
@@ -2,8 +2,24 @@ const CannonType = require("./CannonType");
 module.exports = {
   WAR_FRIG: {
     name: "War Frigate",
-    cannonsPerSide: 2,
+    dualCannon: true,
     maxMoves: 3,
     cannonType: CannonType.LARGE,
+    rockDamage: 1.667,
+    ramDamage: 2,
+    maxDamage: 33.32,
+    influenceDiameter: 8,
+    shipSize: 3,
+  },
+  WAR_BRIG: {
+    name: "War Brig",
+    dualCannon: true,
+    maxMoves: 3,
+    cannonType: CannonType.MEDIUM,
+    rockDamage: 0.833,
+    ramDamage: 1.333,
+    maxDamage: 16.66,
+    influenceDiameter: 6,
+    shipSize: 2,
   },
 };
