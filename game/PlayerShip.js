99c13ce14e1ae3c80673bf3cc7c3c3edc8b8dda8
Mark Artishuk <markyshuk@gmail.com>
2020-05-28 23:43:21 -0700

    Added damage logic to ship

--- game/PlayerShip.js
+++ game/PlayerShip.js
@@ -1,4 +1,5 @@
 const Orientation = require("./Orientation");
+const ShipType = require("./ShipType");
 
 class PlayerShip {
   constructor(id, shipType) {
@@ -11,6 +12,37 @@ class PlayerShip {
     this.damage = 0;
 
     this.orientation = Orientation.SOUTH;
+
+    this.sinking = false;
+
+    // TODO: Maybe a sunk boolean variable?
+  }
+
+  ramRocks() {
+    this.damageShip(this.shipType.rockDamage);
+  }
+
+  /**
+   *
+   * @param {PlayerShip} otherShip  The other player ship that is ramming this ship.
+   */
+  ramShip(otherShip) {
+    otherShip.damageShip(this.shipType.ramDamage);
+    this.damageShip(otherShip.shipType.ramDamage);
+  }
+
+  damageShip(amount) {
+    let total = this.damage + amount;
+
+    if (this.total > amount) this.total = amount;
+
+    this.damage = total;
+
+    if (this._checkSink()) this.sinking = true;
+  }
+
+  _checkSink() {
+    return this.damage >= this.shipType.maxDamage;
   }
 
   /**
