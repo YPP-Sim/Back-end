ae4ec3d08eafec6585cd5a883701206e1931894f
Mark Artishuk <markyshuk@gmail.com>
2020-05-28 23:26:09 -0700

    Added cannon type damage amounts, and converted all damage values to represent small cannonballs

--- game/CannonType.js
+++ game/CannonType.js
@@ -1,5 +1,10 @@
+// Damage is calculated in small cannonballs.
+// MEDIUM and LARGE damage is converted into small types.
 module.exports = {
-  SMALL: "SMALL",
-  MEDIUM: "MEDIUM",
-  LARGE: "LARGE",
+  SMALL: {
+    name: "SMALL",
+    damage: 1,
+  },
+  MEDIUM: { name: "MEDIUM", damage: 1.5 },
+  LARGE: { name: "LARGE", damage: 2 },
 };
