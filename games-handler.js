fda041a49237b997da802a15536c02bb006f76b9
Mark Artishuk <markyshuk@gmail.com>
2020-06-01 22:07:27 -0700

    created games-handler file to handle new/existing games

---
+++ games-handler.js
@@ -0,0 +1,25 @@
+const Game = require("./game/game");
+const JobberQuality = require("./game/JobberQuality");
+
+const games = {};
+
+/**
+ * @param {string} id The unique identifier of the game that will be used to retrieve the instance later
+ * @param { Array.<Array.<number>>} map The map the game will be using
+ * @param { JobberQuality } jobberQuality The quality of jobbers that everyone will get by default
+ */
+function createGame(id, map, jobberQuality) {
+  if (games[id]) throw `Game with id '${id}' already exists`;
+
+  const newGame = new Game(map, jobberQuality);
+  games[id] = newGame;
+}
+
+function getGame(id) {
+  return games[id];
+}
+
+module.exports = {
+  createGame,
+  getGame,
+};
