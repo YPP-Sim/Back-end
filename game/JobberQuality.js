d7ae1d02a87b07da09f0ffc58cdc54f26ef0f542
Mark Artishuk <markyshuk@gmail.com>
2020-09-09 19:21:48 -0700

    Added more stats/detail to JobberQuality

--- game/JobberQuality.js
+++ game/JobberQuality.js
@@ -1,5 +1,21 @@
 module.exports = {
-  BASIC: { name: "Basic" },
-  MEDIUM: { name: "Medium" },
-  ELITE: { name: "Elite" },
+  BASIC: {
+    name: "Basic",
+    fixRate: 0.5,
+    bilgeFixRate: 4,
+    minDamageForBilgePercent: 40,
+    movesPerTurn: 1.7,
+    cannonsPerTurn: 11.5,
+    fullBilgeMoveRate: 0.15,
+  },
+  // MEDIUM: { name: "Medium" },
+  ELITE: {
+    name: "Elite",
+    fixRate: 1,
+    bilgeFixRate: 7,
+    minDamageForBilgePercent: 50,
+    movesPerTurn: 2.4,
+    cannonsPerTurn: 17,
+    fullBilgeMoveRate: 0.3,
+  },
 };
