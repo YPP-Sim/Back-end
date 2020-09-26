973d1bc2a15766e05192f5ba2fc1b3fac41a95e6
Mark Artishuk <markyshuk@gmail.com>
2020-09-25 18:26:03 -0700

    Added HTTPS/SSL cert side of the server

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const dotenv = require("dotenv");
 if (dotenv) dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 const app = express();
 const http = require("http").createServer(app);
+const https = require("https");
 const io = require("socket.io")(http);
 const SocketHandler = require("./SocketHandler");
 
@@ -31,7 +33,9 @@ app.use("/games", gamesRouter);
 app.use("/maps", mapRouter);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Works!" });
+  res
+    .status(200)
+    .json({ message: "Works!", environment: process.env.NODE_ENV });
 });
 
 // --- Server start/listen
@@ -43,3 +47,23 @@ http.listen(port, () => {
       "JWT Key not found, please set a JWT_KEY environment variable"
     );
 });
+
+// HTTPS server setup
+if (process.env.NODE_ENV === "production") {
+  // Certificate
+  const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH, "utf8");
+  const certificate = fs.readFileSync(process.env.CERTIFICATE_PATH, "utf8");
+  const ca = fs.readFileSync(process.env.CA_PATH, "utf8");
+
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+  };
+
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server has been set up and now listening on port 443");
+  });
+}
