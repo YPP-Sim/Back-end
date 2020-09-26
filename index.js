cf79e6aa621eeb962aea74d75739c5c308ea04ec
Mark Artishuk <markyshuk@gmail.com>
2020-09-26 11:56:17 -0700

    Removed https code

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const dotenv = require("dotenv");
 if (dotenv) dotenv.config();
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
 const app = express();
 const http = require("http").createServer(app);
-const https = require("https");
 const io = require("socket.io")(http);
 const SocketHandler = require("./SocketHandler");
 
@@ -47,23 +45,3 @@ http.listen(port, () => {
       "JWT Key not found, please set a JWT_KEY environment variable"
     );
 });
-
-// HTTPS server setup
-if (process.env.NODE_ENV === "production") {
-  // Certificate
-  const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH, "utf8");
-  const certificate = fs.readFileSync(process.env.CERTIFICATE_PATH, "utf8");
-  const ca = fs.readFileSync(process.env.CA_PATH, "utf8");
-
-  const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca,
-  };
-
-  const httpsServer = https.createServer(credentials, app);
-
-  httpsServer.listen(443, () => {
-    console.log("HTTPS Server has been set up and now listening on port 443");
-  });
-}
